58db7c3cb4c1f3eacdd382728e6539d915df37af
nanaf106 <h.elmougharti@gmail.com>
2022-02-28 20:35:30 +0100

    Projet django

--- BLOCKCHAINEXPLORER/BLOCKCHAINEXPLORER/asgi.py
+++ BLOCKCHAINEXPLORER/BLOCKCHAINEXPLORER/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for BlockchainExplorer project.
+ASGI config for BLOCKCHAINEXPLORER project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BlockchainExplorer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BLOCKCHAINEXPLORER.settings')
 
 application = get_asgi_application()
--- BLOCKCHAINEXPLORER/BLOCKCHAINEXPLORER/wsgi.py
+++ BLOCKCHAINEXPLORER/BLOCKCHAINEXPLORER/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for BlockchainExplorer project.
+WSGI config for BLOCKCHAINEXPLORER project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BlockchainExplorer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BLOCKCHAINEXPLORER.settings')
 
 application = get_wsgi_application()
