3c477e68ff6664f8415ef5a1f65bee96e2c66364
Sam <Ibejuniorr@gmail.com>
2022-03-14 19:51:28 +0100

    Ajout recup donnees

--- BLOCKCHAINEXPLORER/BLOCKCHAINEXPLORER/stats.py
+++ BLOCKCHAINEXPLORER/BLOCKCHAINEXPLORER/stats.py
@@ -2,20 +2,81 @@ import urllib.parse
 import requests
 from requests.structures import CaseInsensitiveDict
 
-url = "https://mainnet.infura.io/v3/6e5e803cf1f84b4b85e6533c11f68639"
+url = "https://api.zmok.io/mainnet/lcf0jmfdvhdi3ezt"
 headers = CaseInsensitiveDict()
 headers["Content-Type"] = "application/json"
+adress = "0x9c5083dd4838e120dbeac44c052179692aa5dac5"
 
+
+def getGasPrice() :
+
+    json_data ={"jsonrpc":"2.0","method":"eth_gasPrice","params": [],"id":1}
+    resp = requests.post(url, headers=headers, json = json_data)
+    json = resp.json()
+    result = int(json['result'],16)*10**(-9)
+    return (result)
+    
 def getBlockNumber() :
 
-
-
-    data = '{"jsonrpc":"2.0","method":"eth_blockNumber","params": [],"id":1}'
-
-
-    resp = requests.post(url, headers=headers, data=data)
+    json_data = {"jsonrpc":"2.0","method":"eth_blockNumber","params": [],"id":1}
+    resp = requests.post(url, headers=headers, json = json_data)
     json = resp.json()
     result = int(json['result'],16)
     return (result)
 
-print(getBlockNumber())
+def getBalanceEth(Walletadress) :
+
+    json_data ={"jsonrpc" : "2.0", "method" : "eth_getBalance", "params" : [ Walletadress , "latest"],"id" : 1}
+    resp = requests.post(url, headers=headers, json = json_data)
+    json = resp.json()
+    result = int(json['result'],16)*10**(-18)
+    return (result)
+
+def getBlock(BlockNumber) :
+    json_data = {"jsonrpc":"2.0","method":"eth_getBlockByNumber","params": [BlockNumber,False],"id":1}
+    resp = requests.post(url, headers=headers, json = json_data)
+    json = resp.json()
+    result = json['result']['transactions']
+    return (result)
+
+def getTransactionCount(blockNumber) :
+    json_data = {"jsonrpc":"2.0","method":"eth_getBlockTransactionCountByNumber","params": [blockNumber],"id":1}
+    resp = requests.post(url, headers=headers, json = json_data)
+    json = resp.json()
+    result = int(json['result'],16)
+    return (result)
+
+def getBlockInfo(BlockNumber,indexTransaction) :
+    json_data = {"jsonrpc":"2.0","method":"eth_getTransactionByBlockNumberAndIndex","params": [BlockNumber,indexTransaction],"id":1}
+    resp = requests.post(url, headers=headers, json = json_data)
+    json = resp.json()
+    valueInEth = int(json['result']['value'],16)*10**(-18)
+    return valueInEth
+
+def getTransactionInfo(TransactionHash) :
+    json_data = {"jsonrpc":"2.0","method":"eth_getTransactionByHash","params": [TransactionHash],"id":1}
+    resp = requests.post(url, headers=headers, json = json_data)
+    json = resp.json()
+    valueInEth = int(json['result']['value'],16)*10**(-18)
+    return valueInEth
+
+blockNumber = getBlockNumber()
+blockNumberHex = hex(blockNumber)
+numberOfTransactions = getTransactionCount(blockNumberHex)
+index = 0
+InfoBlock = getBlock(blockNumberHex)
+
+
+print("The number of the last block is : " + str(blockNumber))
+print("The actual gas price is : " + str(getGasPrice()) + " Gwei") 
+print("The ETH balance of the adress " + adress + " is : " + str(getBalanceEth(adress)))
+#print("The hash of the transaction number " + str(index) + " in the block number " + str(blockNumber) + " is : " + str(InfoTransactions))
+print("The number of transactions in the block number " + str(blockNumber) + " is : " + str(numberOfTransactions))
+tab = []
+for i in range (50) :
+    print("The transaction number " + str(i) + " named by the hash : " + InfoBlock[i] + " of the block number " + str(blockNumber) +  " has a value of " + str(getTransactionInfo(InfoBlock[i])) + " Ethereum")
+    #tab.append(getTransactionInfo(InfoBlock[i]))
+#print(tab)
+
+
+#print("The transaction has a value of : " +  str(getTransactionInfo(blockNumberHex,"0x0")) + " Ethereum")
