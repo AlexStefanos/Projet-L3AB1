1a71e36c4b7a06a37fb8e1d399e868387c61106e
Sam <Ibejuniorr@gmail.com>
2022-03-21 18:57:23 +0100

    Code rapide et mise en fichier JSON de ce qui nous intéresse

--- BLOCKCHAINEXPLORER/BLOCKCHAINEXPLORER/stats.py
+++ BLOCKCHAINEXPLORER/BLOCKCHAINEXPLORER/stats.py
@@ -1,13 +1,20 @@
 import urllib.parse
 import requests
+import json
+import threading
+import time
 from requests.structures import CaseInsensitiveDict
 
 
 
 url = "https://api.zmok.io/mainnet/lcf0jmfdvhdi3ezt"
+url2 = "https://api.zmok.io/mainnet/uoeoajazlmlsvslh"
+url3 = "https://api.zmok.io/mainnet/swmxlmavvfhtdeyl"
 headers = CaseInsensitiveDict()
 headers["Content-Type"] = "application/json"
 adress = "0x9c5083dd4838e120dbeac44c052179692aa5dac5"
+notReadyBegin = True
+notReadyEnd = True
 
 session = requests.Session()
 session.get(url)
@@ -50,16 +57,9 @@ def getTransactionCount(blockNumber) :
     result = int(json['result'],16)
     return (result)
 
-def getBlockInfo(BlockNumber,indexTransaction) :
-    json_data = {"jsonrpc":"2.0","method":"eth_getTransactionByBlockNumberAndIndex","params": [BlockNumber,indexTransaction],"id":1}
-    resp = session.post(url, headers=headers, json = json_data)
-    json = resp.json()
-    valueInEth = int(json['result']['value'],16)*10**(-18)
-    return valueInEth
-
-def getTransactionInfo(TransactionHash) :
+def getTransactionInfo(TransactionHash,Url) :
     json_data = {"jsonrpc":"2.0","method":"eth_getTransactionByHash","params": [TransactionHash],"id":1}
-    resp = session.post(url, headers=headers, json = json_data)
+    resp = session.post(Url, headers=headers, json = json_data)
     json = resp.json()
     valueInEth = int(json['result']['value'],16)*10**(-18)
     return valueInEth
@@ -67,21 +67,60 @@ def getTransactionInfo(TransactionHash) :
 blockNumber = getBlockNumber()
 blockNumberHex = hex(blockNumber)
 numberOfTransactions = getTransactionCount(blockNumberHex)
-index = 0
 InfoBlock = getBlock(blockNumberHex)
-
 
 print("The number of the last block is : " + str(blockNumber))
 print("The actual gas price is : " + str(getGasPrice()) + " Gwei") 
 print("The ETH balance of the adress " + adress + " is : " + str(getBalanceEth(adress)))
-#print("The hash of the transaction number " + str(index) + " in the block number " + str(blockNumber) + " is : " + str(InfoTransactions))
 print("The number of transactions in the block number " + str(blockNumber) + " is : " + str(numberOfTransactions))
-tab = []
-for i in range (numberOfTransactions) :
 
-    print("The transaction number " + str(i) + " named by the hash : " + InfoBlock[i] + " of the block number " + str(blockNumber) +  " has a value of " + str(getTransactionInfo(InfoBlock[i])) + " Ethereum")
-    #tab.append(getTransactionInfo(InfoBlock[i]))
-#print(tab)
+tabJsonBeginningValue = []
+tabJsonMiddleValue = []
+tabJsonEndValue = []
+tabJsonBeginningHash = []
+tabJsonMiddleHash = []
+tabJsonEndHash = []
+
+def f1():
+    for i in range(0,int(numberOfTransactions/3)) :
+        tabJsonBeginningValue.append(getTransactionInfo(InfoBlock[i],url))
+        tabJsonBeginningHash.append(InfoBlock[i])
+        
+ 
+def f2():
+    for i in range(int(numberOfTransactions/3),int(2*numberOfTransactions/3)) :
+        tabJsonMiddleValue.append(getTransactionInfo(InfoBlock[i],url2))
+        tabJsonMiddleHash.append(InfoBlock[i])
 
 
-#print("The transaction has a value of : " +  str(getTransactionInfo(blockNumberHex,"0x0")) + " Ethereum")
+def f3():
+    for i in range(int(2*numberOfTransactions/3),numberOfTransactions) :
+        tabJsonEndValue.append(getTransactionInfo(InfoBlock[i],url3))
+        tabJsonEndHash.append(InfoBlock[i])
+ 
+
+t1 = threading.Thread(None,target=f1)
+t2 = threading.Thread(None,target=f2)
+t3 = threading.Thread(None,target = f3)
+
+t1.start()
+t2.start()
+t3.start()
+
+debut = time.time()
+
+while (t1.is_alive() or t2.is_alive() or t3.is_alive()) :
+    time.sleep(0.5)
+fin = time.time()
+
+print(fin-debut)
+
+tabJsonBeginningValue.extend(tabJsonMiddleValue)
+tabJsonBeginningValue.extend(tabJsonEndValue)
+
+tabJsonBeginningHash.extend(tabJsonMiddleHash)
+tabJsonBeginningHash.extend(tabJsonEndHash)
+
+jsonString = {"NumberLastBlock" : str(blockNumber), "GasPrice(Gwei)" : str(getGasPrice()), "NbTransactions" : str(numberOfTransactions) , "AllTransactionsHash" : str(tabJsonBeginningHash) , "ValueOfEachTransaction" : str(tabJsonBeginningValue)}
+Json = json.dumps(jsonString)
+print(Json)
