e5b4b6fc53f27ff21fb40573d6237f51d5564544
Arthur <arthur.vallee@gmail.com>
2022-03-28 15:33:36 +0200

    Auto stash before merge of "main" and "origin/main"
    Avancement site

--- BLOCKCHAINEXPLORER/static/js/index.js
+++ BLOCKCHAINEXPLORER/static/js/index.js
@@ -1,4 +1,20 @@
+//const { getJSON } = require("jquery");
+
 function test(){
     var inputs = document.getElementById("ser");
 	console.log(inputs.value);
 }
+
+const request = new XMLHttpRequest();
+request.open("GET", "http://127.0.0.1:8000/api/getExemple");
+request.send();
+request.onload = ()=>{
+    if(request.status === 200){
+        var exemple = JSON.parse(request.response);
+        document.getElementById("gasPrice").innerHTML = exemple['GasPrice(Gwei)'];
+    }
+    else{
+        console.log('error ${request.status}')
+    }
+}
+
