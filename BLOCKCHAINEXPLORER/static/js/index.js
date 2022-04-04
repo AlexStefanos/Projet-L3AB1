3e5ee2dff2c2574f5b98ce9c7611e964d59b8705
root <root@DESKTOP-0EHSRH1.localdomain>
2022-04-04 18:07:26 +0200

    SEARCH BAR FONCTIONNEL

--- BLOCKCHAINEXPLORER/static/js/index.js
+++ BLOCKCHAINEXPLORER/static/js/index.js
@@ -1,15 +1,17 @@
 //const { getJSON } = require("jquery");
 
-function test(){
+function search(){
     var inputs = document.getElementById("ser");
-	console.log(inputs.value);
+	console.log(window.location.href);
+    var text = document.getElementById('ser');
+    console.log(text.value);
+    window.location.href = 'http://127.0.0.1:8000/tx/'.concat(text.value);
 }
 
 function link(numberTransac){
     var text = document.getElementById('transaction'.concat(numberTransac)).textContent;
     document.getElementById('transaction'.concat(numberTransac)).setAttribute("href", "tx/".concat(text));
 }
-
 var LastBlock
 const request = new XMLHttpRequest();
 request.open("GET", "http://127.0.0.1:8000/api/getLastBlock");
@@ -27,11 +29,31 @@ request.onload = ()=>{
     
         if(request2.status === 200){
             var block = JSON.parse(request2.response);
+            var nbTx = block['NumberTransactionsInBlock'];
             var liste =  block['AllTransactionsHash'];
-            var liste2 = liste.split(/[\s'',]+/);
-            for (let i = 1 ; i<16; i++){
-                document.getElementById("transaction".concat(i)).innerHTML = liste2[i];
+            var listeArray = liste.split(/[\s'',]+/);
+            var listePop = listeArray.pop();
+	        var listeShift = listeArray.shift();
+            while(nbTx < 16){
+                LastBlock = LastBlock - 1;
+                const request = new XMLHttpRequest();
+                request.open("GET", "http://127.0.0.1:8000/api/getInfoHashBlock/".concat(LastBlock));
+                request.send();
+                request.onload = ()=>{
+                    if(request.status === 200){
+                        var nbTxT = block['NumberTransactionsInBlock'];
+                        nbTx = nbTx + nbTxT;
+                        var listestr =  block['AllTransactionsHash'];
+                        var listeArray2 = listestr.split(/[\s'',]+/);
+                        var listePop2 = listeArray2.pop();
+	                    var listeShift2 = listeArray2.shift();
+                        listeArray = listeArray.concat(listeArray2);
+                    }
+                }
             }
+            for (let i = 1 ; i<16; i++){
+                document.getElementById("transaction".concat(i)).innerHTML = listeArray[i];
+                     }
 
             //document.getElementById("gasPrice").innerHTML = exemple['GasPrice(Gwei)'];
         }
@@ -45,5 +67,6 @@ request.onload = ()=>{
         console.log('error ${request.status}')
     }
 }
+
 
 
