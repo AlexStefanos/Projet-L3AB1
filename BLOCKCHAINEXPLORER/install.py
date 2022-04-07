a76f73f4a48172392e33709b4c55877e0b3293ac
Alex <alexandrestefanoss@gmail.com>
2022-04-07 16:00:46 +0200

    Finalisations install.py et run.py

--- BLOCKCHAINEXPLORER/install.py
+++ BLOCKCHAINEXPLORER/install.py
@@ -1,9 +1,10 @@
 import os
 
+os.system('sudo apt install python3-pip')
 os.system('pip install djangorestframework')
 os.system('pip install requests')
 os.system('pip install djongo')
-os.system('pip install pymongo=3.12.1')
+os.system('pip install pymongo==3.12.1')
 os.system('sudo apt-get install npm')
 os.system('npm install bootstrap')
 os.system('pip install dnspython')
--- BLOCKCHAINEXPLORER/run.py
+++ BLOCKCHAINEXPLORER/run.py
@@ -3,30 +3,28 @@ import subprocess, time, webbrowser
 import platform
 
 monOS = platform.system()
+pathGlob = glob.glob('datas/*')
+if(os.path.exists('datas')):
+   for f in pathGlob:
+      if(os.path.isfile(f)):
+         os.remove(f)
+      if(os.path.isdir(f)):
+         shutil.rmtree(f)
 if(monOS == 'Linux' or monOS == 'Darwin'):
-   pathGlob = glob.glob('datas/*')
+   processMongoDB = subprocess.Popen(["mongod", "--dbpath", "datas/"])
+processDjango = subprocess.Popen(["python3", "manage.py", "runserver"])
+time.sleep(8)
+webbrowser.open('http://127.0.0.1:8000/')
+close = 0
+while(close != 1):
+   time.sleep(5)
+   close = int(input("Si vous voulez fermer le site, tapez 1\n"))
+if(close == 1):
+   processMongoDB.kill()
+   processDjango.kill()
    if(os.path.exists('datas')):
       for f in pathGlob:
          if(os.path.isfile(f)):
             os.remove(f)
          if(os.path.isdir(f)):
             shutil.rmtree(f)
-   processMongoDB = subprocess.Popen(["mongod", "--dbpath", "datas/"])
-   processDjango = subprocess.Popen(["python3", "manage.py", "runserver"])
-   time.sleep(8)
-   webbrowser.open('http://127.0.0.1:8000/')
-   close = 0
-   while(close != 1):
-      time.sleep(5)
-      close = int(input("Si vous voulez fermer le site, tapez 1\n"))
-   if(close == 1):
-      processMongoDB.kill()
-      processDjango.kill()
-      if(os.path.exists('datas')):
-         for f in pathGlob:
-            if(os.path.isfile(f)):
-               os.remove(f)
-            if(os.path.isdir(f)):
-               shutil.rmtree(f)
-else:
-   print("je suis sur windows")
