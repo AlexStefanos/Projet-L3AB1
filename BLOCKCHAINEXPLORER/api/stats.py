df96aa6c2710fda67e682f9f782b9e79b25050f7
Arthur <arthur.vallee@gmail.com>
2022-04-11 15:13:44 +0200

    update

--- BLOCKCHAINEXPLORER/api/stats.py
+++ BLOCKCHAINEXPLORER/api/stats.py
@@ -216,6 +216,7 @@ def drawTransactionsChart() :
 
     return [x,y]
 
+"""
 def drawTopAdressChart() :
     data = {'code': 1, 'msg': '成功', 'data': [{'addr': '0x00000000219ab540356cbb839cbe05303d7705fa', 'balance': 11352626.000069, 'txCnt': 34838}, {'addr': '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', 'balance': 6340481.331016232, 'txCnt': 3973918}, {'addr': '0xda9dfa130df4de4673b89022ee50ff26f6ea73cf', 'balance': 2113030.0012, 'txCnt': 64}, {'addr': '0xbe0eb53f46cd790cd13851d5eff43d12404d33e8', 'balance': 1996008.2837798258, 'txCnt': 1088}, {'addr': '0x73bceb1cd57c711feac4224d062b0f6ff338501e', 'balance': 1923504.538509494, 'txCnt': 480}, {'addr': '0x9bf4001d307dfd62b26a2f1307ee0c0307632d59', 'balance': 1490000.0180927091, 'txCnt': 103}, {'addr': '0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5', 'balance': 1028541.690326043, 'txCnt': 25020}, {'addr': '0x61edcdf5bb737adffe5043706e7c5bb1f1a56eea', 'balance': 929498.95358134, 'txCnt': 336}, {'addr': '0xdc24316b9ae028f1497c275eb9192a3ea0f67022', 'balance': 784665.045977826, 'txCnt': 12732}, {'addr': '0x011b6e24ffb0b5f5fcc564cf4183c5bbbc96d515', 'balance': 593103.3479250012, 'txCnt': 50}]}
     x = []
@@ -224,7 +225,7 @@ def drawTopAdressChart() :
         y.append(data['data'][i]["balance"])
         x.append(data['data'][i]["addr"])
     return [x,y]
-    
+"""    
 def drawPieTopCrypto() :
     url = "https://api.coingecko.com/api/v3/global"
     response = requests.request('GET', url)
@@ -260,10 +261,11 @@ gasPrice = getGasPrice()
 for data in x:
     dict = data.copy()
 
-if (drawEthChart() != [dict['x_data_EthPrice'],dict['y_data_EthPrice']]) :
+#if (drawEthChart() != [dict['x_data_EthPrice'],dict['y_data_EthPrice']]) :
+if(True):
     (x_data_EthChart,y_data_EthChart) = drawEthChart()
     (x_data_EthTxCt,y_data_EthTxCt) = drawTransactionsChart()
-    (x_data_TopWallet,y_data_TopWallet) = drawTopAdressChart()
+    #(x_data_TopWallet,y_data_TopWallet) = drawTopAdressChart()
     (x_data_PieMC,y_data_PieMc) = drawPieTopCrypto()
 
 
@@ -271,8 +273,8 @@ if (drawEthChart() != [dict['x_data_EthPrice'],dict['y_data_EthPrice']]) :
             "y_data_EthPrice" : y_data_EthChart,
             "x_data_EthTxCt" : x_data_EthTxCt,
             "y_data_EthTxCt" : y_data_EthTxCt,
-            "x_data_TopWallet" : x_data_TopWallet,
-            "y_data_TopWallet" : y_data_TopWallet,
+            #"x_data_TopWallet" : x_data_TopWallet,
+            #"y_data_TopWallet" : y_data_TopWallet,
             "x_data_PieMc" : x_data_PieMC,
             "y_data_PieMc" : y_data_PieMc}
 
