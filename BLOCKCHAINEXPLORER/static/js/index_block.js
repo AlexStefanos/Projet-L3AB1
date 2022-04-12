60baf9a5c0f63f9c6e674ef43ee53cb47e3ee70a
root <root@DESKTOP-0EHSRH1.localdomain>
2022-04-12 20:22:37 +0200

    CORRECTION

--- BLOCKCHAINEXPLORER/static/js/index_block.js
+++ BLOCKCHAINEXPLORER/static/js/index_block.js
@@ -9,6 +9,9 @@ function search(){
             window.location.href = 'http://127.0.0.1:8000/adress/'.concat(text.value);
         }
     }
+    else if(lg.length == 0){
+        window.location.href = 'http://127.0.0.1:8000';
+    }
     else{
         window.location.href = 'http://127.0.0.1:8000/block/'.concat(text.value);
     }
