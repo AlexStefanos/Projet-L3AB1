f608476d3df9100bed11de0a0739362932373c33
Alex <alexandrestefanoss@gmail.com>
2022-04-17 08:16:36 +0200

    Changements sur run.py et install.py

--- run.py
+++ run.py
@@ -3,17 +3,17 @@ import subprocess, time, webbrowser
 import platform
 
 monOS = platform.system()
-pathGlob = glob.glob('datas/*')
-if(os.path.exists('datas')):
+pathGlob = glob.glob('BLOCKCHAINEXPLORER/datas/*')
+if(os.path.exists('BLOCKCHAINEXPLORER/datas')):
    for f in pathGlob:
       if(os.path.isfile(f)):
          os.remove(f)
       if(os.path.isdir(f)):
          shutil.rmtree(f)
 if(monOS == 'Linux' or monOS == 'Darwin'):
-   processMongoDB = subprocess.Popen(["mongod", "--dbpath", "datas/"], stderr = subprocess.DEVNULL)
-   time.sleep(5)
-processDjango = subprocess.Popen(["python3", "manage.py", "runserver"], stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
+   processMongoDB = subprocess.Popen(["mongod", "--dbpath", "BLOCKCHAINEXPLORER/datas/"], stderr = subprocess.DEVNULL)
+   time.sleep(6)
+processDjango = subprocess.Popen(["python3", "BLOCKCHAINEXPLORER/manage.py", "runserver"], stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
 time.sleep(8)
 webbrowser.open('http://127.0.0.1:8000/')
 close = 0
